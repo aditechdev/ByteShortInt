3805eb572bbab8fccfeb91f6ac4ffbd4b7879143
dev adi <aditya.anand156@gmail.com>
2018-04-08 17:31:11 +0530

    Initial commit

---
+++ src/com/imadianand/Main.java
@@ -0,0 +1,12 @@
+package com.imadianand;
+
+public class Main {
+
+    public static void main(String[] args) {
+byte myNewByte = 120;
+        short myNewShort = 32765;
+        int myNewInt =22556;
+        long myNewlong= 50000L + 10L *(myNewByte + myNewInt + myNewShort);
+        System.out.println("my long total =" +myNewlong);
+            }
+}
